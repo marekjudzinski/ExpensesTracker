54f126406c7132749bff0a48e6515f0cd5450c79
Marek <marekju80@gmail.com>
2021-11-26 16:01:52 +0100

    displaying the items based on selected and filtered year

--- src/components/Expenses/Expenseitem.js
+++ src/components/Expenses/Expenseitem.js
@@ -4,20 +4,14 @@ import Card from '../UI/Card';
 import './Expenseitem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-
-  const changeTitle = () => {
-    setTitle(title + 2);
-  }
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}zł</div>
       </div>
-      <button onClick={changeTitle}>Change Title</button>
     </Card>
   )
 }
--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,12 +1,20 @@
 import './NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 
-const NewExpense = () => {
+const NewExpense = (props) => {
+
+  const SaveExpenseDataHandler = (enteredExpenseData) =>{
+    const expenseData = {
+      ...enteredExpenseData,
+      id: Math.random().toString()
+    }
+    props.onAddExpense(expenseData)
+  }
   return (
     <div className="new-expense">
-      <ExpenseForm />
+      <ExpenseForm onSaveExpenseData={SaveExpenseDataHandler}/>
     </div>
-  )
+  ) 
 }
 
 export default NewExpense;
